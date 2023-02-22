46cc3ccfa06e9942075df3f77e1ff4c3406c3b05
Suzette McCanny <smccanny@deloitte.ca>
2023-02-22 11:22:34 -0500

    connect to starwars api

---
+++ src/client.ts
@@ -0,0 +1,8 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache();
+
+export const client = new ApolloClient({
+  uri: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+  cache,
+});
